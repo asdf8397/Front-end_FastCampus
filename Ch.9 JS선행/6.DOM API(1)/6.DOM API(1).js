a35d6a9743a3d62c9f567497ce6e3f1a9c95db2b
asdf8397 <scv2314@naver.com>
2021-11-06 15:27:33 +0900

    2021-11-06 Front-End

--- Ch.9 JS선행/6.DOM API(1)/6.DOM API(1).js
+++ Ch.9 JS선행/6.DOM API(1)/6.DOM API(1).js
@@ -5,3 +5,21 @@
 let boxEl = document.querySelector('.box');
 
 console.log(boxEl);
+
+
+
+// HTML 요소(Element) 1개 검색/찾기
+const boxEl = document.querySelector('.box');
+
+// HTML 요소에 적용할 수 있는 메소드!
+boxEl.addEventListener();
+
+// 1 - 이벤트(Event, 상황)
+boxEl.addEventListener('click', 2);
+
+// 2 - 핸들러(Handler, 실행할 함수)
+boxEl.addEventListener('click', function () {
+    console.log('click');
+});
+
+
